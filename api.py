81ec9f9d2f8c62372eb5465219206fa434074d3b
t0665624 <t0665624@d16396-ssd.sefin.fortaleza.ce.gov.br>
2020-01-23 09:55:35 -0300

    Update endpoints

--- api.py
+++ api.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def online():
+    return 'on-line!'
+
+artists = ['Bruno Mars','Jack Johnson','Adele'];
+
+@app.route('/artists', methods=['GET'])
+def get_all():
+    return json.dumps(artists)
+
+@app.route('/artists/<int:index>', methods=['GET'])
+def get_by_index(index):
+    return json.dumps(artists[index])
+
+@app.route('/artists/<int:index>', methods=['DELETE'])
+def delete(index):
+    artists.pop(index)
+    return json.dumps(artists)
+
+@app.route('/artists', methods=['POST'])
+def post():
+    name = request.json['name']
+    artists.append(name)
+    return json.dumps(artists)
+
+@app.route('/artists/<int:index>', methods=['PUT'])
+def put(index):
+    name = request.json['name']
+    artists.insert(index, name)
+    return json.dumps(artists)
